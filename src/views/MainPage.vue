d4ede6cae21db8fb093932be27cedb9bdfa4b807
“RrcardoChaseCo” <jufejxn@163.com>
2024-07-07 23:23:11 +0800

    l

--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -1,16 +1,23 @@
 <template>
-    <div>
-      <div>主页面</div>
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    name: 'MainPage',
-  };
-  </script>
-  
-  <style scoped>
-  /* Add your styles here */
-  </style>
-  
+  <div>
+    <a-calendar v-model:value="value" @panelChange="onPanelChange" />
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+const value = ref();
+const onPanelChange = (value, mode) => {
+  console.log(value, mode);
+};
+</script>
+
+<script>
+export default {
+  name: 'MainPage',
+};
+</script>
+
+<style scoped>
+/* Add your styles here */
+</style>
